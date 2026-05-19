e9a51d13563e4a423eb0ee058ec54925d1164612
Stella Mdr <90699815+StellaMdr@users.noreply.github.com>
2026-05-19 10:30:38 +0200

    Done 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    if (!Number.isInteger(numA) || !Number.isInteger(numB) || numA < 0 || numB < 0) {
+        return "ERROR";
+    }
+    sumNumbers = 0;
+    const min = Math.min(numA, numB);
+    const max = Math.max(numA, numB);
+    for (let i = min; i <= max; i++) {
+        sumNumbers += i;
+    }
+    return sumNumbers;
 
 };
 
